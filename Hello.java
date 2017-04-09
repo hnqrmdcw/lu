c13df0e9ab152aca50a199542ae7a574d8a67938
lujinxiong <824494376@qq.com>
2017-04-09 10:02:49 +0800

    master add

--- Hello.java
+++ Hello.java
@@ -2,10 +2,8 @@ public class Hello
 {
 	private String name;
 	public static void main (String args[]){
-		System.out.println("happy new year");
-		System.out.println("happy myself！");
+		System.out.println("**************");
+		System.out.println("#############");
 	}
-	public static void print(){
-	System.out.println("hello world");
-	}
+
 }
